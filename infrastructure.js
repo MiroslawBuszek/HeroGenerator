72872093263c287439403fa5f6cf82146f5d89c2
Mirosław Buszek <zoowett@gmail.com>
2019-09-18 13:02:06 +0200

    Add read random word function from article content

--- infrastructure.js
+++ infrastructure.js
@@ -3,8 +3,17 @@ const fs = require('fs');
 function readNames(nameList) {
   const content = fs.readFileSync('./content/' + nameList, 'utf8');
   const nameFrequencies = content.split('\r\n');
-  const records = nameFrequencies.map(nf => nf.split(' - '))
+  const records = nameFrequencies.map(nf => nf.split(' - '));
   return records;
 }
 
+function readWordSource() {
+  const content = fs.readFileSync('./content/article.txt', 'utf8');
+  return content
+    .split(/\s/)
+    .filter(w => w != false)
+    .filter(w => w.length > 4);
+}
 module.exports.readNames = readNames;
+module.exports.readWordSource = readWordSource;
+ 
